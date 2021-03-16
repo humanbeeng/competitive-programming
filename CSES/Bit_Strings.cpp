b3411b90f2b0a370841413029ee567f01a1072b5
Nithin SJ <thenithinraj@gmail.com>
2021-03-16 21:35:27 +0530

    Solved Problems

---
+++ CSES/Bit_Strings.cpp
@@ -0,0 +1,45 @@
+/*
+Author : humanbeeng
+*/
+#include <bits/stdc++.h>
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define pb(num) push_back(num)
+#define ppb pop_back()
+#define all(arr) arr.begin(), arr.end()
+using namespace std;
+typedef vector<int> vi;
+#define pi(arr) for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++) cout << arr[i] << ' '
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef vector<ii> vii;
+typedef vector<char> vc;
+typedef unsigned long long ull;
+typedef set<string, int> ssi;
+typedef set<int> si;
+typedef set<string> ss;
+#define imn INT_MIN
+#define imx INT_MAX
+#define mod 1000000007
+
+
+// void setio(string s){
+//     ios_base::sync_with_stdio(0); cin.tie(0);
+//     freopen((s+".in").c_str(),"r",stdin);
+//     freopen((s+".out").c_str(),"w",stdout);
+// }
+
+
+int main(){
+    int n;
+    cin >>n;
+    int res=1;
+    fo(i, n){
+        res = (res * 2)%mod;
+    }
+    cout << res;
+    
+
+
+
+    return 0;
+}
--- CSES/Weird_Algorithm.cpp
+++ CSES/Weird_Algorithm.cpp
@@ -2,15 +2,18 @@
 Author : humanbeeng
 */
 #include <bits/stdc++.h>
+using namespace std;
 #define fo(i, n) for (int i = 0; i < n; i++)
 #define pb(num) push_back(num)
 #define ppb pop_back()
 #define all(arr) arr.begin(), arr.end()
-using namespace std;
-typedef vector<int> vi;
 #define pi(arr)                                            \
     for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) \
     cout << arr[i] << ' '
+#define imn INT_MIN
+#define imx INT_MAX
+#define mod 1000000007
+typedef vector<int> vi;
 typedef pair<int, int> ii;
 typedef long long ll;
 typedef vector<ii> vii;
@@ -19,8 +22,6 @@ typedef unsigned long long ull;
 typedef set<string, int> ssi;
 typedef set<int> si;
 typedef set<string> ss;
-#define imn INT_MIN;
-#define imx INT_MAX;
 
 void setio(string s) {
     ios_base::sync_with_stdio(0);
@@ -30,29 +31,17 @@ void setio(string s) {
 }
 
 int main() {
-    int n;
+    ll n;
     cin >> n;
-    int arr[n + 1];
-    for (int i = 1; i <= n; i++) {
-        int num;
-        cin >> num;
-        arr[i] = num;
-    }
-    int res = abs(arr[1] - arr[n]);
-    int resi = 1;
-    int resj = n;
-    int x = 1;
-    int y = 2;
-    while (y <= n) {
-        if (res > abs(arr[x] - arr[y])) {
-            res = abs(arr[x] - arr[y]);
-            resi = x;
-            resj = y;
+    while (n != 1) {
+        cout << n << " ";
+        if (n % 2 == 0) {
+            n = (n >> 1);
+        } else if (n % 2 == 1) {
+            n = ((n * 3) + (1));
         }
-        x++;
-        y++;
     }
-    cout << resi << " " << resj;
+    cout << n;
 
     return 0;
 }
