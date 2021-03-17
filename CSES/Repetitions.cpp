b904a486ffb8ee3254bc795716ea7a8d8745e6e9
Nithin SJ <thenithinraj@gmail.com>
2021-03-17 09:20:21 +0530

    Solved Problems

---
+++ CSES/Repetitions.cpp
@@ -0,0 +1,56 @@
+/*
+Author : humanbeeng
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define pb(num) push_back(num)
+#define ppb pop_back()
+#define all(arr) arr.begin(), arr.end()
+#define pi(arr)                                            \
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) \
+    cout << arr[i] << ' '
+#define imn INT_MIN
+#define imx INT_MAX
+#define mod 1000000007
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef vector<ii> vii;
+typedef vector<char> vc;
+typedef unsigned long long ull;
+typedef set<string, int> ssi;
+typedef set<int> si;
+typedef set<string> ss;
+
+void setio(string s) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+
+int main() {
+
+    int res = imn;
+    string str;
+    getline(cin, str);
+    char init = str.back();
+    str.pop_back();
+    int count = 1;
+    while(str.size() != 0){
+        char c = str.back();
+        if(c == init){
+            count++;
+            res = max(count, res);
+        }else{
+            count = 1;
+            init = c;
+        }
+        str.pop_back();
+    }
+    if(res < count) res = 1;
+    cout << res;
+
+    return 0;
+}
