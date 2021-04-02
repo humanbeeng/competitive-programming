9ffe6109089bf8e98fc2f060bf4bca4fda28f20d
Nithin <thenithinraj@gmail.com>
2021-04-02 22:12:28 +0530

    Solved Problems

---
+++ CSES/B_Partial_Replacement.cpp
@@ -0,0 +1,80 @@
+/*
+Author : humanbeeng
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(i, init, n) for (int i = init; i < n; i++)
+#define pb(num) push_back(num)
+#define ppb pop_back()
+#define all(arr) arr.begin(), arr.end()
+#define pi(arr) for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++) cout << arr[i] << ' '
+#define imn INT_MIN
+#define imx INT_MAX
+#define mod 1000000007
+#define ssz(arrayy) sizeof(arrayy)/sizeof(arrayy[0])
+#define sf(num) scanf("%d", &num)
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef vector<ii> vii;
+typedef vector<char> vc;
+typedef unsigned long long ull;
+typedef set<string, int> ssi;
+typedef set<int> si;
+typedef set<string> ss;
+
+
+void setio(string s){
+    freopen((s+".in").c_str(),"r",stdin);
+    freopen((s+".out").c_str(),"w",stdout);
+}
+
+int solve(){
+    int n,k;
+    cin >> n >> k;
+    char str[n];
+    bool flagfirst = true;
+    int firststar;
+    int laststar;
+    fo(i, 0, n){
+        char c;
+        cin >> c;
+        str[i] = c;
+        if(c == '*' && flagfirst){
+            firststar = i;
+            laststar = i;
+            flagfirst = false;
+        }else if(c == '*' && flagfirst == false){
+            laststar = i;
+        }
+        
+    }
+
+    str[firststar] = 'x';
+    str[laststar] = 'x';
+
+    
+
+    
+
+    
+
+
+
+
+    return 0;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    //setio("");
+    int t=1; 
+    cin >> t;
+    while(t--){
+    solve();
+    }
+
+
+
+    return 0;
+}
