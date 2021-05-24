c14456041b7ab066958dd30894e1742c88aa55d3
Nithin <thenithinraj@gmail.com>
2021-05-24 12:23:32 +0530

    Added mor solutions

---
+++ CP Sheet/A_Football.cpp
@@ -0,0 +1,75 @@
+/*
+Author : humanbeeng
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(i, init, n) for (int i = init; i < n; i++)
+#define pb(num) push_back(num)
+#define ppb pop_back()
+#define all(arr) arr.begin(), arr.end()
+#define pi(arr) for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++) cout << arr[i] << ' '
+#define imn INT_MIN
+#define imx INT_MAX
+#define mod 1000000007
+#define ssz(arrayy) sizeof(arrayy)/sizeof(arrayy[0])
+#define sf(num) scanf("%d", &num)
+#define br "\n"
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef vector<ii> vii;
+typedef vector<char> vc;
+typedef unsigned long long ull;
+typedef set<string, int> ssi;
+typedef set<int> si;
+typedef set<string> ss;
+
+
+void setio(string s){
+    freopen((s+".in").c_str(),"r",stdin);
+    freopen((s+".out").c_str(),"w",stdout);
+}
+
+int solve(){
+    int n;
+    cin >> n;
+    string a, b;
+    int goals_a = 0, goals_b = 0;
+    fo(i, 0, n){
+        
+        string str;
+        
+        cin >> str;
+        if(i==0){
+            a = str;
+            goals_a++;
+        }else if( str != a){
+            b = str;
+            goals_b++;
+        }else{
+            goals_a++;
+        }
+        
+        
+    }
+    goals_a > goals_b ? cout << a : cout << b;
+
+
+
+
+    return 0;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    //setio("");
+    int t=1; 
+    //cin >> t;
+    while(t--){
+    solve();
+    }
+
+
+
+    return 0;
+}
