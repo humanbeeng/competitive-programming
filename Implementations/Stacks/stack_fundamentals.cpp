5c5545f4315137e82277b1a7bafc033be60c3648
Nithin <thenithinraj@gmail.com>
2021-07-23 17:12:14 +0530

    Added many algorithms of Stack into one class

--- Implementations/Stacks/stack_fundamentals.cpp
+++ Implementations/Stacks/stack_fundamentals.cpp
@@ -8,12 +8,14 @@ class StackFundamentals {
 private:
     vector<int> arr;
     int n_elements;
+    vector<int> res;
 
 public:
     StackFundamentals(int n) {
+        res.clear();
+        arr.clear();
         n_elements = n;
         arr = _take_input(n);
-        _nearest_greater_right();
         // _print_arr();
     }
 
@@ -27,8 +29,8 @@ private:
         return arr;
     }
 
-private:
-    void _nearest_greater_right() {
+public:
+    void nearest_greater_right() {
         vector<int> res;
         stack<int> st;
 
@@ -50,16 +52,94 @@ private:
             st.push(arr[i]);
         }
         reverse(res.begin(), res.end());
-        for (int item : res) {
-            cout << item << " ";
+
+        print_res(res);
+    }
+
+public:
+    void nearest_greater_left() {
+        vector<int> res;
+        stack<int> st;
+
+        for (int i = 0; i < n_elements; i++) {
+            if (st.empty()) {
+                res.push_back(-1);
+            } else if (!st.empty() && st.top() > arr[i]) {
+                res.push_back(st.top());
+            } else if (!st.empty() && st.top() <= arr[i]) {
+                while (!st.empty() && st.top() <= arr[i]) {
+                    st.pop();
+                }
+                if (st.empty()) {
+                    res.push_back(-1);
+                } else if (st.top() > arr[i]) {
+                    res.push_back(st.top());
+                }
+            }
+            st.push(arr[i]);
         }
+
+        print_res(res);
+
+        //NO REVERSING OF RES
+    }
+
+public:
+    void nearest_smaller_right() {
+        vector<int> res;
+        stack<int> st;
+
+        for (int i = n_elements - 1; i >= 0; i--) {
+            if (st.empty()) {
+                res.push_back(-1);
+            } else if (!st.empty() && st.top() < arr[i]) {
+                res.push_back(st.top());
+            } else if (!st.empty() && st.top() >= arr[i]) {
+                while (!st.empty() && st.top() >= arr[i]) {
+                    st.pop();
+                }
+                if (st.empty())
+                    res.push_back(-1);
+                else if (st.top() < arr[i]) {
+                    res.push_back(st.top());
+                }
+            }
+            st.push(arr[i]);
+        }
+        print_res(res);
+    }
+
+public:
+    void nearest_smaller_left() {
+        vector<int> res;
+        stack<int> st;
+
+        for (int i = 0; i < n_elements; i++) {
+            if (st.empty()) {
+                res.push_back(-1);
+            } else if (!st.empty() && st.top() < arr[i]) {
+                res.push_back(st.top());
+            } else if (!st.empty() && st.top() >= arr[i]) {
+                while (!st.empty() && st.top() >= arr[i]) {
+                    st.pop();
+                }
+                if (st.empty()) {
+                    res.push_back(-1);
+                } else if (st.top() < arr[i]) {
+                    res.push_back(st.top());
+                }
+            }
+            st.push(arr[i]);
+        }
+        //NO REVERSING
+        print_res(res);
     }
 
     //debug place
 
-private:
-    void _print_arr() {
-        for (int item : arr) {
+public:
+    void print_res(vector<int> res) {
+        for (int item : res) {
             cout << item << " ";
         }
         cout << "\n";
@@ -67,7 +147,9 @@ private:
 };
 
 int main() {
-    StackFundamentals ngr = StackFundamentals(5);
+    StackFundamentals stack_fundamentals = StackFundamentals(5);
+    stack_fundamentals.nearest_smaller_left();
+
     // ngr.print_res();
     return 0;
 }
