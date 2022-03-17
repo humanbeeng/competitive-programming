764b1d232714d459a202c6144b538a551783b4a9
Nithin <thenithinraj@gmail.com>
2022-03-17 22:14:59 +0530

    Add Dynamic Programming Solutions

--- Implementations/Dynamic Programming/3_Knapsack_Tabulation.cpp
+++ Implementations/Dynamic Programming/3_Knapsack_Tabulation.cpp
@@ -7,44 +7,31 @@ using namespace std;
 int t[1002][1002];
 int knapsack_tabulation(int wt[], int val[], int W, int n) {
     memset(t, -1, sizeof(t));
-    /* 
-	Recursion = base condition + induction + hypothesis
-	similarly here base condition = initialization
-	the two changing variables are W and n
-	hence make a matrix of that depicting the results of two
-	variables. 
-	 */
+    /*
+    Recursion = base condition + induction + hypothesis
+    similarly here base condition = initialization
+    the two changing variables are W and n
+    hence make a matrix of that depicting the results of two
+    variables.
+     */
 
-    //Step 1 : Initialization
-
-    for (int i = 0; i < n + 1; i++) {
-        for (int j = 0; j < W + 1; j++) {
-            if (i == 0)
+    // Step 1 : Initialization
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= W; j++) {
+            if (i == 0 || j == 0) {
                 t[i][j] = 0;
-            if (j == 0)
-                t[i][j] = 0;
+            }
         }
     }
 
     // Step 2 : solving for smaller inputs
 
-    for (int i = 1; i < n + 1; i++) {
-        for (int j = 1; j < W + 1; j++) {
-            // here i = n and j = W
-            /* 
-			in recursive code we had three cases 
-			if(n == 0 || W == 0) return 0;
-			else if(wt[n - 1] <= W)
-			return max(val[n - 1] + recursive_knapsack(wt, val, W - wt[n - 1], n - 1), recursive_knapsack(wt, val, W, n - 1));
-			else return recursive_knapsack(wt, val, W, n - 1);
-			
-
-			We have already taken care of base condition
-			 */
-            if (wt[i - 1] <= j) {
-                t[i][j] = max(val[i - 1] + t[i - 1][j - wt[i - 1]], t[i - 1][j]);
-            } else if (wt[i - 1] > j) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= W; j++) {
+            if (wt[i - 1] > j) {
                 t[i][j] = t[i - 1][j];
+            } else {
+                t[i][j] = max(val[i - 1] + t[i - 1][j - wt[i - 1]], t[i - 1][j]);
             }
         }
     }
