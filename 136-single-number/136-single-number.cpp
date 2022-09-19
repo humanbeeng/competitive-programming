ca9a9c96edf47c2d3068b2f4b292c8cd76b0bfbd
Nithin SJ <thenithinraj@gmail.com>
2022-09-19 21:42:51 +0530

    Time: 34 ms (54.55%), Space: 16.9 MB (86.31%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int curr_xor = nums[0];
+        
+        for(int i = 1; i < nums.size(); i++) {
+            curr_xor ^= nums[i];
+        }
+        
+        return curr_xor;
+    }
+};
